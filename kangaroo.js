e765b5754330d714de6d809a05efd86dee1f6506
sriley <sarahriley.dev@gmail.com>
2020-01-02 20:08:49 -0500

    kangaroo

---
+++ kangaroo.js
@@ -0,0 +1,6 @@
+// Complete the function kangaroo in the editor below. It should return YES if they reach the same position at the same time, or NO if they don't.
+
+// kangaroo has the following parameter(s):
+
+// x1, v1: integers, starting position and jump distance for kangaroo 1
+// x2, v2: integers, starting position and jump distance for kangaroo 2
