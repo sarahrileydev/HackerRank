a44cc91114e03e9053fb42e754de7837ef5c4228
sriley <sarahriley.dev@gmail.com>
2020-01-19 19:36:49 -0500

    viralAdevertising

---
+++ viralAdvertising.js
@@ -0,0 +1,14 @@
+function viralAdvertising(n) {
+  let daysAdvertised = 0;
+  let shareStart = 5;
+  let likes = 0;
+  let totalLikes = 0;
+
+  while (daysAdvertised <= n){
+      likes = 
+    daysAdvertised++;
+  }
+  console.log(likes)
+}
+
+viralAdvertising(5)
